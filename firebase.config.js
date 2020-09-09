fff1b9d0f212c912f7a631d86dde7451cb503c98
Rashmi V Abbigeri <41290852+Rashmi-278@users.noreply.github.com>
2020-09-09 19:53:01 +0530

    Update firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	apiKey: "",
-	authDomain: "",
-	databaseURL: "",
-	storageBucket: "",
+	apiKey: "AIzaSyDp93kKv9a5QU-eQGy7fqzikXedlJ08M6k",
+	authDomain: "test-auth-module.firebaseapp.com",
+	databaseURL: "https://test-auth-module.firebaseio.com",
+	storageBucket: "test-auth-module.appspot.com",
 }
